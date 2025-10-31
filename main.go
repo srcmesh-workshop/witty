af49f9a9e2859fb5be0e05ed961d7c722296fa96
Ta-Ching Chen <tachingchen@srcmesh.com>
2025-10-31 22:48:33 +0800

    wip

--- main.go
+++ main.go
@@ -108,7 +108,7 @@ func main() {
 		//	cmdToExec = []string{"bash"}
 		//}
 
-		cmdToExec = []string{"ssh", "127.0.0.1 -l $LOGIN_USER -o StrictHostKeyChecking=no"}
+		cmdToExec = []string{"ssh", "${LOGIN_USER}@127.0.0.1 -o StrictHostKeyChecking=no"}
 
 		options.CmdToExec = cmdToExec
 
